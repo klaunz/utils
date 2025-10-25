3f32047bdbaef100e516abebe342d88b198bc068
Kinson Lau <klauiwi@gmail.com>
2025-10-25 21:30:44 +1300

    Update to TS and rename file

--- debounce.ts
+++ debounce.ts
@@ -1,18 +1,24 @@
-function debounce(fn, delay = 500) {
-  let timeoutId, lastArgs
+/**
+ * Debounce
+ * @param {Function} fn
+ * @param {Number} delay
+ * @returns Function
+ */
+function debounce(fn: Function, delay = 500) {
+  let timeoutId: number | undefined, lastArgs: any[]
 
-  function debounceFn(...args) {
+  function debounceFn(...args: any[]) {
     lastArgs = args
     clearTimeout(timeoutId)
     timeoutId = setTimeout(() => {
-      timeoutId = null
+      timeoutId = undefined
       fn(...args)
     }, delay)
   }
 
   debounceFn.cancel = () => {
     clearTimeout(timeoutId)
-    timeoutId = null
+    timeoutId = undefined
   }
 
   debounceFn.flush = () => {
